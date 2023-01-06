7e661262b7ec0d6f65a13c59bf7f9e906d129adc
Andrew Burkhart <Burkhartfc@iCloud.com>
2023-01-06 10:33:26 -0700

    feat: Add ViewModel to ShoeDetail

--- app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeDetailFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
+import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.viewmodels.ShoeViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -16,6 +20,7 @@ import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
  */
 class ShoeDetail : Fragment() {
     private lateinit var binding: FragmentShoeDetailBinding
+    private lateinit var viewModel: ShoeViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,14 +32,37 @@ class ShoeDetail : Fragment() {
             container,
             false
         )
+        viewModel = ViewModelProvider(requireActivity()).get(ShoeViewModel::class.java)
+
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
         binding.saveButton.setOnClickListener { v: View ->
             // TODO: Save shoe
-            v.findNavController().navigate(ShoeDetailDirections.actionShoeDetailToShoeListFragment())
+            if (viewModel.validate()) {
+                viewModel.addShoeToList(
+                    Shoe(
+                        name = viewModel.shoe.name,
+                        size = viewModel.shoe.size,
+                        company = viewModel.shoe.company,
+                        description = viewModel.shoe.description
+                    )
+                )
+                v.findNavController()
+                    .navigate(ShoeDetailDirections.actionShoeDetailToShoeListFragment())
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Check that all fields have been filled",
+                    Toast.LENGTH_LONG
+                )
+                    .show()
+            }
         }
 
         binding.cancelButton.setOnClickListener { v: View ->
-            v.findNavController().navigate(ShoeDetailDirections.actionShoeDetailToShoeListFragment())
+            v.findNavController()
+                .navigate(ShoeDetailDirections.actionShoeDetailToShoeListFragment())
         }
 
         return binding.root
--- app/src/main/java/com/udacity/shoestore/viewmodels/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/viewmodels/ShoeViewModel.kt
@@ -6,8 +6,25 @@ import androidx.lifecycle.ViewModel
 import com.udacity.shoestore.models.Shoe
 
 class ShoeViewModel : ViewModel() {
+    val shoe = Shoe("", 0.0, "", "")
 
     private val _shoesList = MutableLiveData<MutableList<Shoe>>()
     val shoesList: LiveData<MutableList<Shoe>>
         get() = _shoesList
+
+    fun addShoeToList(newShoe: Shoe) {
+        // TODO: Figure out Double for size
+        if (_shoesList.value == null) {
+            _shoesList.value = mutableListOf(shoe)
+        } else {
+            _shoesList.value?.add(newShoe)
+        }
+    }
+
+    fun validate(): Boolean {
+        return this.shoe.name.isNotEmpty() &&
+                this.shoe.size.isFinite() &&
+                this.shoe.company.isNotEmpty() &&
+                this.shoe.description.isNotEmpty()
+    }
 }
